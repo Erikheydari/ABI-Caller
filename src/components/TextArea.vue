ee4967880d1a89a803a899d8544d62b75214b3d7
erikheydari <erikheydari@gmail.com>
2025-07-14 18:12:46 +0330

    Add TextArea component for user input
    - Introduced a new TextArea.vue component that allows users to input multi-line text with customizable properties including label, placeholder, modelValue, disabled state, and number of rows.
    - Implemented two-way data binding for the modelValue prop to facilitate input updates.
    - Added scoped styles for the textarea appearance and transitions to enhance user experience.

---
+++ src/components/TextArea.vue
@@ -0,0 +1,78 @@
+<script setup>
+defineProps({
+    label: {
+        type: String,
+        required: true
+    },
+    placeholder: {
+        type: String,
+        default: ''
+    },
+    modelValue: {
+        type: String,
+        default: ''
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    rows: {
+        type: Number,
+        default: 4
+    }
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <div class="textarea-wrapper">
+        <label :for="label">{{ label }}</label>
+        <textarea 
+            class="default-transition" 
+            :id="label" 
+            :placeholder="placeholder" 
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)" 
+            :disabled="disabled"
+            :rows="rows" 
+        />
+    </div>
+</template>
+
+<style scoped>
+.default-transition {
+    transition: all 0.3s ease;
+}
+
+.textarea-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: start;
+    width: 100%;
+    gap: 0.5rem;
+}
+
+textarea {
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid var(--color-input-border);
+    border-radius: 0.5rem;
+    background-color: var(--color-input-background);
+    color: var(--color-text);
+    outline: none;
+    resize: vertical;
+    font-family: inherit;
+
+    &:focus {
+        border-color: var(--color-input-border-focus);
+        color: var(--color-text-active);
+    }
+
+    &:disabled {
+        opacity: var(--opacity-disabled);
+        cursor: not-allowed;
+    }
+}
+</style> 
