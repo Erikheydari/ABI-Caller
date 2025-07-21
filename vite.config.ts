f42e78287226e7cfeeb7966355a6520164888c4a
erikheydari <erikheydari@gmail.com>
2025-07-21 13:14:21 +0330

    Update base path in Vite configuration and adjust asset links in index.html for root deployment
    - Changed the base path in vite.config.ts from '/ABI-Caller/' to '/' for root deployment.
    - Updated asset links in dist/index.html to reflect the new base path, ensuring correct loading of favicon, JavaScript, and CSS files.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/ABI-Caller/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
